79835feb9c8f2b757722e614c4d8968cdc4a6333
Andrea Storace <a.storace@wikicasa.it>
2020-04-05 18:14:17 +0200

    added path to webpack.devs.js and webpack.prod.js

--- src/main/web/webpack.devs.js
+++ src/main/web/webpack.devs.js
@@ -1,5 +1,6 @@
 const common = require('./webpack.common');
 const merge = require('webpack-merge');
+const path = require('path');
 
 module.exports = merge(common, {
     mode : 'development',
--- src/main/web/webpack.prod.js
+++ src/main/web/webpack.prod.js
@@ -1,6 +1,7 @@
 const TerserPlugin = require('terser-webpack-plugin');
 const common = require('./webpack.common');
 const merge = require('webpack-merge');
+const path = require('path');
 
 module.exports = merge(common, {
     mode : 'production',
