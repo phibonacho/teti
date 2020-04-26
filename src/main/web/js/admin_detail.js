1c996cb4b7c57911034bbf4d8b8ad5b62d13fcda
Andrea Storace <a.storace@wikicasa.it>
2020-04-26 21:20:50 +0200

    playing around with overlays

--- src/main/web/js/admin_detail.js
+++ src/main/web/js/admin_detail.js
@@ -1,15 +1,20 @@
 // SCSS
 import "../scss/admin_detail.scss";
 
+// JS
+import "./components/fontawesome"
+
 // VUE
 import "./vue/side-bar";
 import asyncTable from "./vue/async-table"
 import Vue from "vue";
-import {FormPlugin, FormInputPlugin} from "bootstrap-vue";
+import axios from 'axios';
+import {FormPlugin, FormInputPlugin, OverlayPlugin} from "bootstrap-vue";
 
 Vue.component('async-table', asyncTable);
 Vue.use(FormPlugin);
 Vue.use(FormInputPlugin);
+Vue.use(OverlayPlugin);
 
 
 new Vue({
@@ -29,13 +34,43 @@ new Vue({
                 city : ''
             }
         },
+        saveOverlay: false,
+        save : {
+            businessName : '',
+            fiscalCode : '',
+            phone : '',
+            mobilePhone : '',
+            fax : '',
+            note : '',
+            address : {
+                street : '',
+                streetNumber : '',
+                zipCode : '',
+                city : ''
+            }
+        },
     },
     methods : {
         reloadData (event) {
             event.preventDefault();
-            console.log("reloading data using filters: ");
-            console.log(this.search);
-            this.$root.$emit('bv::refresh::table', 'adm-table')
+            this.$root.$emit('bv::refresh::table', 'adm-table');
+        },
+        saveData (event) {
+            event.preventDefault();
+            this.saveOverlay = true;
+            let promise = axios.post('/adm-api/save', this.save)
+            promise.then(response => {
+                // clean form...
+                this.$root.$emit('bv::refresh::table', 'adm-table');
+            }).catch(response => {
+                console.log(response);
+
+            }).then(response => {
+                this.saveOverlay = false;
+            });
+        },
+        editData (event) {
+
         }
     }
 });
--- src/main/web/js/vue/async-table.vue
+++ src/main/web/js/vue/async-table.vue
@@ -1,27 +1,61 @@
 <template>
-    <b-table striped hover
+    <b-table hover
              :items="provider"
              :fields="fields"
              :current-page="1"
-             :per-page="20"
+             :per-page="10"
              :api-url="fetchPath"
              id="adm-table">
+        <template v-slot:cell(id)="data">
+            <div class="row justify-content-around">
+                <a class="py-1 px-2 remove-administrator" @click="checkDelete(data.value)"><i class="fas fa-trash-alt text-ed"></i></a>
+                <a class="py-1 px-2 edit-administrator" href="#" :data-id="data.value"><i class="fas fa-pen text-primary"></i></a>
+                <a class="py-1 px-2" :href="'/registries/administrators/' + data.value + '/detail'"><i class="fas fa-portrait text-dark"></i></a>
+            </div>
+        </template>
         <template v-slot:table-busy>
             <div class="text-center text-primary my-2">
                 <b-spinner class="align-middle"></b-spinner>
                 <strong>Loading...</strong>
             </div>
         </template>
+            <template v-slot:overlay>
+                <b-overlay :show="processing.status" no-wrap>
+                    <template>
+                        <div class="text-center text-primary my-2">
+                            <b-spinner class="align-middle"></b-spinner>
+                            <strong>Eseguo ...</strong>
+                        </div>
+                    </template>
+                    <div
+                            v-if="processing.status"
+                            ref="dialog"
+                            tabindex="-1"
+                            role="dialog"
+                            aria-modal="false"
+                            aria-labelledby="form-confirm-label"
+                            class="text-center p-3">
+                        <p><strong id="form-confirm-label">{{ processing.prompt }}</strong></p>
+                        <div class="d-flex">
+                            <b-button variant="outline-danger" class="mr-3" @click="dismiss">
+                                Cancel
+                            </b-button>
+                            <b-button variant="outline-success" @click="execute">OK</b-button>
+                        </div>
+                    </div>
+                </b-overlay>
+            </template>
     </b-table>
 </template>
 
 <script>
     import Vue from 'vue';
-    import {TablePlugin, SpinnerPlugin} from 'bootstrap-vue';
+    import {TablePlugin, SpinnerPlugin, OverlayPlugin} from 'bootstrap-vue';
     import axios from "axios";
 
     Vue.use(TablePlugin);
     Vue.use(SpinnerPlugin);
+    Vue.use(OverlayPlugin);
 
     export default {
         name: "async-table",
@@ -51,9 +85,18 @@
                         formatter : (value) => [value.street, value.streetNumber, value.zipCode, value.city]
                             .filter(elem => !!elem)
                             .join(', ')
+                    },
+                    {
+                        key : 'id',
+                        label : ''
                     }
                 ],
                 isBusy: true,
+                processing : {
+                    status : false,
+                    prompt : '',
+                    target : undefined
+                },
                 items : [],
             }
         },
@@ -70,11 +113,27 @@
                     this.isBusy = !this.isBusy;
                 else
                     this.isBusy = state;
+            },
+            checkDelete(id) {
+                console.log(`trying to delete ${id}!`);
+                this.processing.prompt=`Sei sicuro di voler eliminare l\'amministratore ${id}?`;
+                this.processing.target=id;
+                this.processing.status=true;
+            },
+            execute() {
+                let promise = axios.delete(`/adm-api/${this.processing.target}/delete`);
+                promise.then(response => {
+                    console.log("done!");
+                }).catch(response => {
+                    console.log(response);
+                }).then(response => {
+                    this.dismiss();
+                });
+            },
+            dismiss() {
+                this.processing.target = undefined;
+                this.processing.status = false;
             }
         },
     }
 </script>
-
-<style scoped>
-
-</style>
